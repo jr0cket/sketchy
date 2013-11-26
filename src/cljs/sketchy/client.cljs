1070198342f4b762ba1d1aa5ce84fa75d28a8dc0
Henry Garner <henry@henrygarner.com>
2013-11-26 17:22:59 +0000

    Log mousemove channel

--- src/cljs/sketchy/client.cljs
+++ src/cljs/sketchy/client.cljs
@@ -9,36 +9,16 @@
 (defn events [el type]
   (let [out (chan)]
     (d/listen! el type
-               (fn [e]
-                 (put! out e)
-                 (.preventDefault e)))
+      (fn [e]
+        (put! out e)
+        (.preventDefault e)))
     out))
 
-(defn e->v [e]
-  [(.-x e) (.-y e)])
-
-(defn draw-point [canvas [x y]]
-  (let [context (.getContext canvas "2d")]
-    (.fillRect context x y 10 10)))
-
 (defn main []
-  (let [canvas (sel1 :#canvas)
-        move (map< e->v (events canvas "mousemove"))
-        down (events canvas "mousedown")
-        up (events canvas "mouseup")]
-    (go
-     (let [ws (<! (ws-ch "ws://localhost:3000/ws"))]
-       (go-loop []
-         (draw-point canvas (read-string (:message (<! ws))))
-         (recur))
-       (go-loop [draw-point? false]
-         (let [[v sc] (alts! [move down up])]
-           (condp = sc
-             down (recur true)
-             up (recur false)
-             move (do (when draw-point?
-                        (js/console.log (pr-str v))
-                        (>! ws v))
-                      (recur draw-point?)))))))))
+  (let [mouse-move (events js/window "mousemove")]
+    (go-loop []
+      (js/console.log (<! mouse-move))
+      (recur))))
 
 (set! (.-onload js/window) main)
+
